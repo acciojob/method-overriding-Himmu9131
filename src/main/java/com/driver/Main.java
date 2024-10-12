3d827d14bac8e2b78c6d80eaa1b2704f2c70b27c
Himmu9131 <114733178+Himmu9131@users.noreply.github.com>
2024-10-12 19:32:39 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
     B b = new B();
-    String s=b.meth();
-        System.out.println( s);
+   b.meth();
+        
 }
 }
